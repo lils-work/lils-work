2e615f32b4e75dc6504f17faff8a097ff1826332
lymims <lymims@outlook.com>
2023-01-25 18:32:10 +0800

    添加主页、路由、图标

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import Home from './pages/Home.vue';
+import { createRouter, createWebHistory } from 'vue-router'
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '', component: Home },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+createApp(App)
+  .use(router)
+  .mount('#app')
---
+++ src/pages/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="root">
+  <br>
+    <br>
+    <img src="../assets/logo.png" alt="lil's" style="width: 6rem" class="w-24">
+    <br>
+    <h1 class="text-3xl font-sans">Work</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped lang="scss">
+.root {
+  font-family: Inter, Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+}
+</style>
